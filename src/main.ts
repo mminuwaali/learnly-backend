e5efb3da9c8f460c5fcef74e8b5636c4141ed31d
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-08-09 16:03:16 +0100

    setup media url

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,21 @@
 import "dotenv/config";
+import cors from "cors";
+import path from 'path';
+import express from "express";
 import sequelize from "./sequelize";
 import account from "./modules/account";
 import product from "./modules/product";
-import express, { NextFunction, Response } from "express";
 import { jwtMiddleware } from "./middlewares/token.midleware";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(errorMiddleware);
 app.use<any>(jwtMiddleware);
-
-app.use((_, res: Response, next: NextFunction): void => {
-    res.header("Access-Control-Allow-Origin", `http://localhost:${port}`);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    next();
-});
+app.use('/media', express.static(path.join(__dirname, '../media')));
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", account.routers, product.routers);
